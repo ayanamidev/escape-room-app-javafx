e2947fd497a9952b14b9d6c4726c9c6ef6d807fb
LauraDev <lauralodeirodev@gmail.com>
2024-10-18 10:41:44 +0200

    Set background

--- src/main/java/com/example/escaperoomfx/EscapeRoomApplication.java
+++ src/main/java/com/example/escaperoomfx/EscapeRoomApplication.java
@@ -11,7 +11,7 @@ public class EscapeRoomApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EscapeRoomApplication.class.getResource("escape-room-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Escape Room");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/escaperoomfx/EscapeRoomController.java
+++ src/main/java/com/example/escaperoomfx/EscapeRoomController.java
@@ -2,10 +2,41 @@ package com.example.escaperoomfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
 
 public class EscapeRoomController {
+    public ImageView logo;
+    public ImageView background;
+    public StackPane logoContainer;
+    public AnchorPane logoPosition;
     @FXML
     private Label welcomeText;
+
+    @FXML
+    public void initialize() {
+        /*
+        Image image = new Image(String.valueOf(getClass().getResource("images/logoo.jpg")));
+        logo.setImage(image);
+
+        Rectangle clip = new Rectangle(
+                logo.getFitWidth(), logo.getFitHeight()
+        );
+        clip.setArcWidth(30);
+        clip.setArcHeight(30);
+        logo.setClip(clip);*/
+
+        Image backgroundImage = new Image(String.valueOf(getClass().getResource("images/background.jpg"))); // Cambia la ruta a tu imagen
+        background.setImage(backgroundImage);
+        /*
+        AnchorPane.setTopAnchor(logo, 10.0);
+        AnchorPane.setLeftAnchor(logo, 10.0);
+        */
+
+    }
 
     /*
     @FXML
