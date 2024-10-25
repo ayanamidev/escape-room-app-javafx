849bf1827f831a1af287d3bc7de8941e603d8fe5
LauraDev <lauralodeirodev@gmail.com>
2024-10-25 09:57:39 +0200

    Add logic for the try button: Display remaining attempts on click

--- src/main/java/com/example/escaperoomfx/EscapeRoomController.java
+++ src/main/java/com/example/escaperoomfx/EscapeRoomController.java
@@ -1,6 +1,8 @@
 package com.example.escaperoomfx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -13,8 +15,11 @@ public class EscapeRoomController {
     public ImageView background;
     public StackPane logoContainer;
     public AnchorPane logoPosition;
+    public Label numberOfAttempts;
+    public Button tryButton;
     @FXML
     private Label welcomeText;
+    int numAttemps=6;
 
     @FXML
     public void initialize() {
@@ -32,10 +37,23 @@ public class EscapeRoomController {
 
         Image backgroundImage = new Image(String.valueOf(getClass().getResource("images/background.jpg"))); // Cambia la ruta a tu imagen
         background.setImage(backgroundImage);
+
+        numberOfAttempts.setText(String.valueOf(numAttemps));
         /*
         AnchorPane.setTopAnchor(logo, 15.0);
         AnchorPane.setLeftAnchor(logo, 15.0);
         */
+
+    }
+
+    public void onTryButtonClick(ActionEvent actionEvent) {
+
+        numAttemps--;
+        numberOfAttempts.setText(String.valueOf(numAttemps));
+
+        if (numAttemps<=0){
+            tryButton.setDisable(true);
+        }
 
     }
 
